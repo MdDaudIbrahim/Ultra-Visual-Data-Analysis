3b451e752f40e413c2d84777c8fc0a4a14ced0d5
Daud Ibrahim <daudibrahim029@gmail.com>
2025-08-20 01:36:28 +0600

    Add streamlit_app.py entry point for Streamlit Cloud

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+"""
+Streamlit Cloud Entry Point for Ultra-Visual Excel Data Analyzer
+This file imports and runs the main ultra_visual_analyzer.py
+"""
+
+# Import the main analyzer
+from ultra_visual_analyzer import *
+
+# The main() function from ultra_visual_analyzer.py will be called automatically
+# when this file is run by Streamlit Cloud
